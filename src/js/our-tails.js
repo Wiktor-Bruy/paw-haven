475dc3c1d7a28a80d81bba6bbe59db51f804bc6a
Crazy_Fox <holmyliszag@ukr.net>
2025-12-14 12:22:25 +0200

    list categories

--- src/js/our-tails.js
+++ src/js/our-tails.js
@@ -1,21 +1,40 @@
 import axios from 'axios';
 
 //-----------------------------------------------------------------Глобальні-змінні
-let tailCategories;
 //---------------------------------------------------------------------------------
 //--------------------------------------------------------------------------Функції
 function tailsGetCategories() {
   axios
     .get('https://paw-hut.b.goit.study/api/categories')
     .then(res => {
-      console.log(res);
+      const tailCategories = res.data.toSorted((a, b) =>
+        a._id.localeCompare(b._id)
+      );
+      tailsRenderCategories(tailCategories);
     })
     .catch(err => {
-      console.log(err);
+      alert(
+        `Вибачте, нажаль при запиті сталася помилка ${err}. Спробуйте знов.`
+      );
     });
+}
+function tailsRenderCategories(arr) {
+  let gallery = [];
+
+  arr.forEach(elem => {
+    const buttuon = document.createElement('button');
+    buttuon.classList.add('tails-select');
+    buttuon.textContent = elem.name;
+    buttuon.setAttribute('data-id', `${elem._id}`);
+    buttuon.setAttribute('type', 'button');
+    gallery.push(buttuon);
+  });
+
+  const btnList = document.querySelector('.tails-button-list');
+  btnList.append(...gallery);
 }
 
 function tailsGetArrAnimals() {}
 //---------------------------------------------------------------------------------
-//------------------------------------------------------------------Загальна-логіка
-tailCategories();
+//------------------------------------------------------------------Загальна-логікa
+tailsGetCategories();
